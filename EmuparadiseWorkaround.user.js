2913f2f4bec6cd7c3b49e73ddf6a0d320e7bd562
CptBrian <CptBrian@users.noreply.github.com>
2020-05-13 10:45:47 -0500

    Update EmuparadiseWorkaround.user.js

--- EmuparadiseWorkaround.user.js
+++ EmuparadiseWorkaround.user.js
@@ -12,8 +12,5 @@
     'use strict';
 
     var id = ((document.URL).split("/"))[5];
-    $(".download-link").prepend(`<a target="_blank" href="50.7.136.26/may/Mags/get-download.php?gid=`+id+`&test=true" title="Download using the workaround script">Download using the workaround script</a><br><br>`);
-    $(".download-link").prepend(`<a target="_blank" href="50.7.136.26/Mags/get-download.php?gid=`+id+`&test=true" title="Download using the workaround script">Download using the workaround script</a><br><br>`);
-    $(".download-link").prepend(`<a target="_blank" href="50.7.136.26/Guides/get-download.php?gid=`+id+`&test=true" title="Download using the workaround script">Download using the workaround script</a><br><br>`);
     $(".download-link").prepend(`<a target="_blank" href="/roms/get-download.php?gid=`+id+`&test=true" title="Download using the workaround script">Download using the workaround script</a><br><br>`);
 })();
