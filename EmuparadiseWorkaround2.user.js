e439009a23bc0c02cd8c6f0f1394f5c1f3baf9d4
CptBrian <CptBrian@users.noreply.github.com>
2020-05-13 07:25:30 -0500

    Update EmuparadiseWorkaround2.user.js

--- EmuparadiseWorkaround2.user.js
+++ EmuparadiseWorkaround2.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name	EmuParadise Download Workaround - 0.0.1
-// @version	0.0.1
+// @name	EmuParadise Download Workaround 1
+// @version	1
 // @description	Replaces the download button link with a working one
 // @author	strupo (based on Epton (https://gist.github.com/Eptun/3fdcc84552e75e452731cd4621c535e9/))
 // @match	https://www.emuparadise.me/*/*/*
